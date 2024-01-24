ca58f486692b783b32b11e2dd5e45308ae037706
Tnkma <onwusilikegodswill@gmail.com>
2024-01-24 23:32:54 +0100

    installed the flask and created a manifest that kills a process

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,5 @@
 # Kills a process called killmwnow using puppet
 exec { 'killmenow_process':
-  command     => 'pkill -f killmenow',
-  path        => ['/bin', '/usr/bin'],
-  onlyif      => 'pgrep -f killmenow',
-  refreshonly => true,
+  command => 'pkill -f killmenow',
+  path    => ['/bin', '/usr/bin'],
 }
