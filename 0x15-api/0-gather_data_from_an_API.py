e25445ad172b1ad05d74179007c06b4bac8aade8
Tnkma <onwusilikegodswill@gmail.com>
2024-02-20 09:25:01 +0100

    fixed pycodestyle

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,25 +3,33 @@
 import requests
 import sys
 
-def get_todo_list_progress(employee_id):
+
+def list_progress(employee_id):
+
     """
     gets the todo_list progress
-    returns the completed and total list 
+    returns the completed and total list
     """
-    response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
+
+    todo_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
+    response = requests.get(todo_url)
     todo_list = response.json()
-    name_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
+
+    name_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    name_response = requests.get(name_url)
     employee_name = name_response.json()
+
     # Get the total number of todo_list
     total_todo_list = len(todo_list)
-    completed_inthe_list = [task for task in todo_list if task['completed']]
+    completed_list = [task for task in todo_list if task['completed']]
 
-    return employee_name['name'], completed_inthe_list, total_todo_list
+    return employee_name['name'], completed_list, total_todo_list
 
 
 if __name__ == '__main__':
     employee_id = sys.argv[1]
-    name, completed_todos, total_todos = get_todo_list_progress(employee_id)
-    print(f"Employee {name} is done with tasks({len(completed_todos)}/{total_todos}):")
-    for task in completed_todos:
+    name, compl_todos, total_todos = list_progress(employee_id)
+    dtask = len(compl_todos)
+    print(f"Employee {name} is done with tasks({dtask}/{total_todos}):")
+    for task in compl_todos:
         print(f"\t {task['title']}")
