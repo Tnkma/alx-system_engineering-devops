9355a5aab53336ebdd874bac9cc15393e34f13b0
Tnkma <onwusilikegodswill@gmail.com>
2024-02-22 20:30:24 +0100

    all todo_list

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,38 +1,31 @@
 #!/usr/bin/python3
-" Gets the data from API and export it to json"
+"""Gets the data from API and exports it to json"""
 import json
 import requests
-import sys
 
 
 def list_progress(employee_id):
-    " A function to get the employee data"
-
+    """A function to get the employee data"""
     tod_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
     response = requests.get(tod_url)
     todo_list = response.json()
 
     name_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
     name_response = requests.get(name_url)
-    employee_name = name_response.json()
+    employee_name = name_response.json()['username']
 
     completed_list = [task for task in todo_list if task['completed']]
-    return employee_name['username'], completed_list, todo_list
+    return employee_name, completed_list, todo_list
 
 
 if __name__ == "__main__":
-    c_args = sys.argv[1]
-    username, completed, todo_list = list_progress(c_args)
-
-    # json_file = f'{c_args}.json'
-    data = {
-        c_args: [
-            {"username": username, "task": task['title'],
-             "completed": task['completed']}
-            for task in todo_list
-        ]
-    }
+    all_employee_data = {}
+    for employee_id in range(1, 11):
+        username, completed, todo_list = list_progress(employee_id)
+        user_tasks = [{"username": username, "task": task['title'],
+                       "completed": task['completed']} for task in todo_list]
+        all_employee_data[str(employee_id)] = user_tasks
 
     with open('todo_all_employees.json', 'w') as file:
-        json.dump(data, file)
+        json.dump(all_employee_data, file)
         # Use indent for pretty formatting
