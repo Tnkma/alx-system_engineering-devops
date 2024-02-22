7ccdcd6087579ed9ed4f6ecee024368f3479fef1
Tnkma <onwusilikegodswill@gmail.com>
2024-02-22 19:38:35 +0100

    exports to json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+" Gets the data from API and export it to json"
+import json
+import requests
+import sys
+
+
+def list_progress(employee_id):
+    " A function to get the employee data"
+
+    tod_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
+    response = requests.get(tod_url)
+    todo_list = response.json()
+
+    name_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    name_response = requests.get(name_url)
+    employee_name = name_response.json()
+
+    completed_list = [task for task in todo_list if task['completed']]
+    return employee_name['username'], completed_list, todo_list
+
+
+if __name__ == "__main__":
+    c_args = sys.argv[1]
+    username, completed, todo_list = list_progress(c_args)
+
+    json_file = f'{c_args}.json'
+    data = {
+        c_args: [
+            {"task": task['title'], "completed": task['completed'],
+             "username": username}
+            for task in todo_list
+        ]
+    }
+
+    with open(json_file, 'w') as file:
+        json.dump(data, file)
+        # Use indent for pretty formatting
