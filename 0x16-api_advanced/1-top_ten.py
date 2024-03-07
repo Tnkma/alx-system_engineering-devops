410885268a6d8d843f4cee791d4572b16bfa1192
Tnkma <onwusilikegodswill@gmail.com>
2024-03-07 14:52:23 +0100

    10 Hot post

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,16 @@
+import requests
+
+def top_ten(subreddit):
+    """ Requests the API and print None if subreddit is not valid """
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    url = 'https://www.reddit.com/r/{}/hot/.json?limit=10'.format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    try:
+        data = response.json()
+        for post in data['data']['children']:
+            hot_posts = post['data']['title']
+            print(hot_posts)
+
+    except Exception:
+        print(None)
