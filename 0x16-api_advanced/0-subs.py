01f8629226b9a2b67f720a04ede7747350b163dc
Tnkma <onwusilikegodswill@gmail.com>
2024-04-17 12:49:03 +0100

    adjusted the control

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,11 +9,11 @@ def number_of_subscribers(subreddit):
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     respond = requests.get(url, headers=headers, allow_redirects=False)
     # If the status code is not 200, return 0
-    if respond.status_code != 200:
-        return 0
-    returned_json = respond.json()
-    # get the subscribers without the data=key
-    subscribers_count = returned_json['data']['subscribers']
-    # return respond.json().get('data').get('subscribers')
-    # we can also make use of this to return it
-    return subscribers_count
+    if respond.status_code == 200:
+        returned_json = respond.json()
+        # get the subscribers without the data=key
+        subscribers_count = returned_json['data']['subscribers']
+        # return respond.json().get('data').get('subscribers')
+        # we can also make use of this to return it
+        return subscribers_count
+    return 0
