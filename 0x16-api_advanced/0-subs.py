f43d8a924ad5f3f8d37ffc2a772fcb07a6378d9d
Tnkma <onwusilikegodswill@gmail.com>
2024-04-17 14:15:50 +0100

    adjusted the control

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,13 +7,10 @@ def number_of_subscribers(subreddit):
     """ Gets the number of subscribers for a given subreddit"""
     headers = {'User-Agent': 'Mozilla/5.0'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    respond = requests.get(url, headers=headers, allow_redirects=False)
-    returned_json = respond.json()
+    response = requests.get(url, headers=headers, allow_redirects=False)
     # If the status code is not 200, return 0
-    if respond.status_code == 200:
-        # get the subscribers without the data=key
-        subscribers_count = returned_json['data']['subscribers']
-        # return respond.json().get('data').get('subscribers')
-        # we can also make use of this to return it
-        return subscribers_count
+    if response.status_code != 404:
+        result = response.json().get('data')
+        return result.get('subscribers')
     return 0
+
